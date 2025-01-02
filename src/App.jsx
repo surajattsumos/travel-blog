225075d1ad441db96d41f277e083e70344031608
Suraj <suraj_bhandari@tsumos.com>
2025-01-03 01:04:25 +0900

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Headerfile";
+import Entry from "./components/Entry";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <Header />
+      <Entry />
+    </div>
+  );
+}
---
+++ src/components/Headerfile.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="header-container">
+      <img width="40" src="/src/assets/images/globe.png" alt="globe" />
+      <h1>My Travel Journal</h1>
+    </div>
+  );
+}
