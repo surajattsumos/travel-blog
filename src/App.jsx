91689d93be81060871be1076a691e7e7e3039c10
Suraj <suraj_bhandari@tsumos.com>
2025-01-03 16:20:47 +0900

    mapped data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
 import Header from "./components/Headerfile";
 import Entry from "./components/Entry";
+import { blogdata } from "./assets/blogdata";
 
 export default function App() {
+  const blogelements = blogdata.map((item) => {
+    return (
+      <Entry
+        key={item.title}
+        img={item.img}
+        location={item.location}
+        map={item.mapUrl}
+        dates={item.dates}
+        description={item.description}
+      />
+    );
+  });
+
   return (
     <div className="app-container">
       <Header />
-      <Entry />
+      {blogelements}
     </div>
   );
 }
---
+++ src/assets/blogdata.js
@@ -0,0 +1,54 @@
+export const blogdata = [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    dates: "12 Jan, 2021 - 24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    img: {
+      src: "https://scrimba.com/links/travel-journal-japan-image-url",
+      alt: "Mount Fuji",
+    },
+    mapUrl:
+      "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
+  },
+  {
+    title: "Geirangerfjord",
+    location: "Norway",
+    dates: "01 Oct, 2021 - 18 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    img: {
+      src: "https://scrimba.com/links/travel-journal-norway-image-url",
+      alt: "Geirangerfjord",
+    },
+    mapUrl:
+      "https://www.google.com/maps/place/Geirangerfjord/@62.1049113,7.0051741,12z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi",
+  },
+  {
+    title: "Sydney Opera House",
+    location: "Australia",
+    dates: "27 May, 2021 - 8 Jun, 2021",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+    img: {
+      src: "https://scrimba.com/links/travel-journal-australia-image-url",
+      alt: "Sydney Opera House",
+    },
+    mapUrl:
+      "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2152967,15z/data=!4m2!3m1!1s0x0:0x3133f8d75a1ac251",
+  },
+  {
+    title: "Eiffel Tower",
+    location: "Paris",
+    dates: "15 Mar, 2021 - 22 Mar, 2021",
+    description:
+      "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.",
+    img: {
+      src: "https://scrimba.com/links/travel-journal-norway-image-url",
+      alt: "Eiffel Tower",
+    },
+    mapUrl:
+      "https://www.google.com/maps/place/Eiffel+Tower/@48.8583701,2.2944813,17z",
+  },
+];
