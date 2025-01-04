06b261ead812b01a87c5d42fb6c6ee05db6255f8
kyoka-euph <108689712+kyoka-euph@users.noreply.github.com>
2025-01-05 01:25:45 +0900

    🚚 clean up the project structure. (#2)

---
+++ blog-frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Headerfile";
+import Entry from "./components/Entry";
+import { blogApi } from "../../blog-backend/services/api";
+export function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogData = async () => {
+      try{
+        const data = await blogApi.getAllPosts();
+        setBlogs(data);
+      } catch (error) {
+        console.error('Error fetching blog data', error);
+      }
+    };
+    fetchBlogData();
+  }, []);
+
+  const blogelements = blogs.map((item) => {
+    return (
+      <Entry
+        key={item.title}
+        img={item.img}
+        location={item.location}
+        map={item.mapUrl}
+        dates={item.dates}
+        description={item.description}
+      />
+    );
+  });
+
+  return (
+    <div className="app-container">
+      <Header />
+      {blogelements}
+    </div>
+  );
+}
